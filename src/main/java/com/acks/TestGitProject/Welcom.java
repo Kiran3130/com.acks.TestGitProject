79f34c9d11ffa02f7b2579b5869324d10e83cb8a
unknown <kiranpatekar3130@gmail.com>
2022-08-25 10:36:34 +0530

    Create com.acks.TestGitProject

---
+++ src/main/java/com/acks/TestGitProject/Welcom.java
@@ -0,0 +1,12 @@
+package com.acks.TestGitProject;
+
+public class Welcom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Kiran Patekar");
+
+	}
+
+}
