3604dc9e32cc3c39d9a0ad53040192187dd7cd0c
Dell <kiranpatekar3130@gmail.com>
2022-08-25 11:48:18 +0530

    Added

--- src/main/java/com/acks/TestGitProject/Name.java
+++ src/main/java/com/acks/TestGitProject/Name.java
@@ -5,7 +5,7 @@ public class Name {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Kiran Patekar");
+		System.out.println("Kiran Patekar ");
 		System.out.println("Patekar");
 
 	}
